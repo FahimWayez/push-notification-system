9d3f63037d087cd13bdfcde274c4c1b5d38ed577
Fahim Wayez <fahimwayez1@gmail.com>
2025-04-11 11:20:56 +0600

    final push done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { PushModule } from './modules/push/push.module';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: Number(process.env.REDIS_PORT) || 6379,
+      },
+    }),
+    UserModule,
+    PushModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor() {
+  //   console.log(
+  //     `Redis running on ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  //   );
+  // }
+}
